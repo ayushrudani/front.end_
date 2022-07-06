766b1d5d1ae4c8e20a792c1ca52c5e384b9e55fa
Ayush Rudani <ayushrudani09@gmail.com>
2022-07-07 00:03:58 +0530

    Add files via upload

---
+++ #106_QR_code/app.js
@@ -0,0 +1,12 @@
+function QR() {
+    document.querySelector('#qr-image').style.display = 'block';
+    let QRtext = document.querySelector('#text').value;
+    if (QRtext.trim().length == 0) {
+        document.querySelector('')
+        document.querySelector('#qr-image').style.display = 'none';
+    } else {
+        document.querySelector('#img').style.display = 'block';
+        document.querySelector('#qr-image .error').innerHTML = '';
+        document.querySelector('#img').src = 'https://api.qrserver.com/v1/create-qr-code/?data=' + QRtext + '&size=200x200';
+    }
+}
